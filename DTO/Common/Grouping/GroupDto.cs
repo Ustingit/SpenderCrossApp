e6f07e2a7e246e97d72fd0517b9878d6d120f4a6
yuryi.ustinovich <ustinhtc@gmail.com>
2022-10-21 18:20:41 +0300

    add common grouping dtos

---
+++ DTO/Common/Grouping/GroupDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Spender.DTO.Common.Grouping
+{
+	public class GroupDto<TKey, TItem>
+	{
+		public GroupDto()
+		{
+		}
+
+		public GroupDto(IEnumerable<IGrouping<TKey, TItem>> data)
+		{
+			Items = data.Select(x => new GroupedItems<TKey, TItem>(x.Key, x.ToList())).ToList();
+		}
+
+		public GroupDto(IEnumerable<TItem> data, Func<TItem, TKey> getter)
+		{
+			Items = data.GroupBy(getter).Select(x => new GroupedItems<TKey, TItem>(x.Key, x.ToList())).ToList();
+		}
+
+		public List<GroupedItems<TKey, TItem>> Items { get; private set; }
+	}
+}
---
+++ DTO/Common/Grouping/GroupedItems.cs
@@ -0,0 +1,15 @@
+﻿namespace Spender.DTO.Common.Grouping
+{
+    public class GroupedItems<TKey, TItem>
+    {
+        public GroupedItems(TKey group, List<TItem> items)
+        {
+            Group = group;
+            Items = items;
+        }
+
+        public TKey Group { get; private set; }
+
+        public List<TItem> Items { get; private set; }
+    }
+}
